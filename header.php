28055667cf976101a30ca8d7188014aa83b05e34
Ulises Viña <playerlgam@live.com>
2020-11-29 13:10:17 -0600

    Arreglé el código en index, limpié el CSS y comentarios en archivos PHP.

--- header.php
+++ header.php
@@ -1,24 +1,13 @@
-<?php
-/**
- * The header for our theme.
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package bafra
- */
-
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta charset="<?php bloginfo("charset"); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <meta charset="utf-8">
 
 <link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="pingback" href="<?php bloginfo("pingback_url"); ?>">
 <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
 
 <?php wp_head(); ?>
@@ -29,4 +18,4 @@
 <?php get_sidebar(); ?>
 
 <div id="page" class="site">
-	<div id="content" class="site-content">
+<div id="content" class="site-content">
